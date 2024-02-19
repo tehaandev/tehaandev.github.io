38693e094fc7daf7b08ad3b26f7f33c03febf69b
Tehaan Perera <134434896+raven8119@users.noreply.github.com>
2024-02-19 15:28:33 +0530

    route testing

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,22 @@ import { lazy, Suspense } from "react";
 import ReactGA from "react-ga";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import About from "./components/About/About.jsx";
+import Contact from "./components/Contact/Contact.jsx";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import Home from "./components/Home/Home.jsx";
 import Loading from "./components/Loading/Loading";
+import Portfolio from "./components/Portfolio/Portfolio.jsx";
+import Skills from "./components/Skills/Skills.jsx";
 import Tabs from "./components/Tabs/Tabs";
 
 // Lazy Components
-const Home = lazy(() => import("./components/Home/Home.jsx"));
-const About = lazy(() => import("./components/About/About.jsx"));
-const Skills = lazy(() => import("./components/Skills/Skills.jsx"));
-const Portfolio = lazy(() => import("./components/Portfolio/Portfolio.jsx"));
-const Contact = lazy(() => import("./components/Contact/Contact.jsx"));
+// const Home = lazy(() => import("./components/Home/Home.jsx"));
+// const About = lazy(() => import("./components/About/About.jsx"));
+// const Skills = lazy(() => import("./components/Skills/Skills.jsx"));
+// const Portfolio = lazy(() => import("./components/Portfolio/Portfolio.jsx"));
+// const Contact = lazy(() => import("./components/Contact/Contact.jsx"));
 
 const HOME_ROUTE = "/*";
 const ABOUT_ROUTE = "/about";
@@ -28,15 +33,15 @@ function App() {
   return (
     <>
       <Header />
-      <Suspense fallback={<Loading className="spinner-container" />}>
-        <Routes>
-          <Route path={HOME_ROUTE} element={<Home />} />
-          <Route path={ABOUT_ROUTE} element={<About />} />
-          <Route path={SKILLS_ROUTE} element={<Skills />} />
-          <Route path={PORTFOLIO_ROUTE} element={<Portfolio />} />
-          <Route path={CONTACT_ROUTE} element={<Contact />} />
-        </Routes>
-      </Suspense>
+      <Routes>
+        {/* <Suspense fallback={<Loading className="spinner-container" />}> */}
+        <Route path={HOME_ROUTE} element={<Home />} />
+        <Route path={ABOUT_ROUTE} element={<About />} />
+        <Route path={SKILLS_ROUTE} element={<Skills />} />
+        <Route path={PORTFOLIO_ROUTE} element={<Portfolio />} />
+        <Route path={CONTACT_ROUTE} element={<Contact />} />
+        {/* </Suspense> */}
+      </Routes>
       <Footer />
       <Tabs />
     </>
