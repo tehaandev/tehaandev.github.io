20059f51e54d4fd25518918c670509b33d22bc1d
Tehaan Perera <134434896+raven8119@users.noreply.github.com>
2024-02-20 21:54:49 +0530

    content update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -45,10 +45,10 @@ function Home() {
         <h4 className="intro-txt greet-msg-txt">Hello, My name is</h4>
         <h1 className="intro-txt">Tehaan Perera</h1>
         <h3 className="intro-txt">
-          And I'm a <span className="text-tertiary">Developer.</span>
+          And I'm a <span className="text-tertiary">Full-stack Developer.</span>
         </h3>
         <p>
-          I’m a developer and UX/UI enthusiast based in Sri Lanka. Currently I’m
+          I’m a full-stack developer based in Sri Lanka. Currently I’m
           a second-year undergraduate majoring in Computer Science.
         </p>
         <p>
