a516b914e5be8ec0f6d9a899d74a02236cb97917
Tehaan Perera <tehaan.p@gmail.com>
2024-03-19 21:08:43 +0530

    api update

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const publishSns = async (subject, message) => {
   const body = { subject: subject, message: message };
-  const apiURL = "https://tehaandev-github-io-backend.vercel.app/sns";
+  const apiURL = "https://portfolio-backend-indol-mu.vercel.app/sns";
   // const apiURL = "http://localhost:3000/sns";
 
   return await axios
