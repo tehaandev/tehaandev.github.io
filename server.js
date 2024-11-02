32d1bd269f9d78a7b89cfb7c85f0daaa09ec4d2e
Tehaan Perera <tehaan.p@gmail.com>
2024-11-02 17:25:19 +0530

    fix: update __dirname resolution and add Vercel configuration

--- server.js
+++ server.js
@@ -1,27 +1,43 @@
-#!/usr/bin/env node
+// #!/usr/bin/env node
+// import path from "path";
+// import express from "express";
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+
+// const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// const app = express();
+// const root = path.join(__dirname, "build/");
+// app.use(express.static(root));
+// app.use((req, res, next) => {
+//   if (
+//     req.method === "GET" &&
+//     req.accepts("html") &&
+//     !req.is("json") &&
+//     !req.path.includes(".")
+//   ) {
+//     res.sendFile("index.html", { root });
+//   } else next();
+// });
+
+// const PORT = process.env.EXPRESS_PORT || 3000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
 import path from "path";
 import express from "express";
-import { dirname } from "path";
 import { fileURLToPath } from "url";
 
-const __dirname = dirname(fileURLToPath(process.env.PUBLIC_URL));
-
 const app = express();
-const root = path.join(__dirname, "build/");
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const root = path.join(__dirname, "../build");
+
 app.use(express.static(root));
-app.use((req, res, next) => {
-  if (
-    req.method === "GET" &&
-    req.accepts("html") &&
-    !req.is("json") &&
-    !req.path.includes(".")
-  ) {
-    res.sendFile("index.html", { root });
-  } else next();
+app.get("*", (req, res) => {
+  res.sendFile("index.html", { root });
 });
 
-const PORT = process.env.EXPRESS_PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+export default app;
